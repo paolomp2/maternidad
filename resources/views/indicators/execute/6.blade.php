8048766ee35b0dea63183a1f520c50e9653ec652
Jonatan Gonzales <jonatan.gonzales@pucp.pe>
2016-05-07 16:55:32 -0500

    otm

--- resources/views/indicators/execute/6.blade.php
+++ resources/views/indicators/execute/6.blade.php
@@ -6,28 +6,26 @@
 	
 	<!-- DATOS PARA EL CHART -->
 	@if($data['chart']=='true')
-	<?php $data_chart = $data['data_chart'] ?>
-	{!! Form::hidden('labels', $data_chart['labels'], ['id'=>'labels']) !!}
-	{!! Form::hidden('num_months', $data_chart['num_months'], ['id'=>'num_months']) !!}	
-	{!! Form::hidden('year_beg', $data_chart['year_beg'], ['id'=>'year_beg']) !!}
-	{!! Form::hidden('year_end', $data_chart['year_end'], ['id'=>'year_end']) !!}
-	{!! Form::hidden('month_beg', $data_chart['month_beg'], ['id'=>'month_beg']) !!}
-	{!! Form::hidden('month_end', $data_chart['month_end'], ['id'=>'month_end']) !!}
+		<?php $data_chart = $data['data_chart'] ?>
+		{!! Form::hidden('labels', $data_chart['labels'], ['id'=>'labels']) !!}
+		{!! Form::hidden('num_months', $data_chart['num_months'], ['id'=>'num_months']) !!}	
+		{!! Form::hidden('year_beg', $data_chart['year_beg'], ['id'=>'year_beg']) !!}
+		{!! Form::hidden('year_end', $data_chart['year_end'], ['id'=>'year_end']) !!}
+		{!! Form::hidden('month_beg', $data_chart['month_beg'], ['id'=>'month_beg']) !!}
+		{!! Form::hidden('month_end', $data_chart['month_end'], ['id'=>'month_end']) !!}
 
-	@for($i=0;$i<$data_chart['num_months'];$i++)
-
-	@endfor
+		@for($i=0;$i<$data_chart['num_months'];$i++)
+		@endfor
 	@endif
 	<!-- FIN DE DATOS PARA EL CHART -->
     <div class="col-lg-12">
         <h3 class="page-header">{!!$data['report_name'] or "No Name detected"!!}</h3>            
     </div>    
 </div>
-@include('layouts.date_range')
+@include('layouts.date_range_only')
 
 @if($data['chart']=='true')
 	@include('layouts.chart')
-
 
 	<div class="panel panel-default">
 		<div class="panel-heading">
